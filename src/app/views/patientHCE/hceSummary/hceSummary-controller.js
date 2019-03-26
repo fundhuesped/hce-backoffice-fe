e92ce3736ea4679ef74a1d69433ad83d30228cbf
Leo <leonardo.manzella@gmail.com>
2019-03-26 17:09:26 -0300

    WIP created modal controller

--- src/app/views/patientHCE/hceSummary/hceSummary-controller.js
+++ src/app/views/patientHCE/hceSummary/hceSummary-controller.js
@@ -12,8 +12,36 @@
 	    var vm = this;
         vm.canGenerateSummary = canGenerateSummary;
         vm.categories = {};
+
         function canGenerateSummary() {
             return vm.controllerForm.$valid && (vm.categories.evolutions||vm.categories.problems||vm.categories.arv||vm.categories.profilaxis||vm.categories.generalTreatment||vm.categories.laboratories||vm.categories.otherStudies||vm.categories.vaccines);
-        }
+        };
+
+        vm.openHivModal = openHivModal;
+
+        //TODO FIXME change selectedProblem for user id or similar
+        function openHivModal(selectedProblem) {
+            var modalInstance = $uibModal.open({
+            backdrop: 'static',
+            templateUrl: 'app/views/patientHCE/hceSummary/hivDetails.html',
+            size: 'md', //TODO FIXME See if we need to set it bigger
+            controller: 'HivDetailsController',
+            controllerAs: 'HivDetailsController',
+            resolve: {
+                //TODO FIXME replace with user id or similar
+                patientProblem: function () {
+                return selectedProblem;
+                }
+            }
+            });
+            modalInstance.result.then(function (resolution) {
+            if(resolution==='markedError' || resolution==='edited'){
+                searchPatientProblems();
+                if(!HCService.currentEvolution){
+                HCService.getCurrentEvolution();
+                }
+            }
+            });
+        };
     }
 })();
---
+++ src/app/views/patientHCE/hceSummary/hivDetails-controller.js
@@ -0,0 +1,137 @@
+(function(){
+    'use strict';
+    /* jshint validthis: true */
+     /*jshint latedef: nofunc */
+    angular
+    	.module('hce.patientHCE')
+    	.controller('HivDetailsController', hivDetailsController);
+
+	hivDetailsController.$inject = ['$state', 'HCService', 'toastr', 'moment', 'Problem', '$uibModal', '$uibModalInstance', 'PatientProblem', 'SessionService','patientProblem'];
+
+    function hivDetailsController ($state, HCService, toastr, moment, Problem, $uibModal, $uibModalInstance, PatientProblem, SessionService, patientProblem) {
+    	var vm = this;
+    	vm.cancel = cancel;
+    	vm.problem = null;
+    	vm.canSaveNewProblem = canSaveNewProblem;
+    	vm.markAsError = markAsError;
+      vm.save = save;
+      vm.originalProblem = patientProblem;
+      vm.changeStatus = changeStatus;
+      vm.canEdit = canEdit;
+      vm.canBeClosed = canBeClosed;
+      vm.canBeMarkedAsError = canBeMarkedAsError;
+
+      vm.startDateCalendarPopup = {
+        opened: false,
+        altInputFormats: ['d!-M!-yyyy'],
+        updateMaxVal: function (value) {
+          this.options.maxDate = (vm.problem.closeDate?vm.problem.closeDate:new Date());
+        },
+        options: {
+          showWeeks: false,
+          maxDate: new Date()
+        },
+        open : function(){
+          this.opened = true;
+        }
+      };
+
+        vm.closeDateCalendarPopup = {
+        opened: false,
+        updateMinVal: function () {
+          this.options.minDate = (vm.problem.startDate?vm.problem.startDate:new Date());
+        },
+        altInputFormats: ['d!-M!-yyyy'],
+        options: {
+          showWeeks: false,
+          maxDate: new Date()
+        },
+        open : function(){
+          this.opened = true;
+        }
+      };
+
+    	activate();
+
+    	function activate() {
+    		vm.problem = angular.copy(patientProblem);
+
+        vm.closeDateCalendarPopup.options.minDate = vm.problem.startDate;
+        vm.problem.startDate = new Date(vm.problem.startDate + 'T03:00:00');
+        if (vm.problem.closeDate) {
+          vm.problem.closeDate = new Date(vm.problem.closeDate + 'T03:00:00');;
+        }
+    	}
+
+
+    	function cancel() {
+    		$uibModalInstance.dismiss('cancel');
+    	}
+
+
+      function save() {
+        var tmpProblem = angular.copy(vm.problem);
+        tmpProblem.startDate = moment(tmpProblem.startDate).format('YYYY-MM-DD');
+        if (tmpProblem.closeDate) {
+            tmpProblem.closeDate = moment(tmpProblem.closeDate).format('YYYY-MM-DD');
+        }
+        PatientProblem.update(tmpProblem, function (response) {
+          toastr.success('Problema editado con éxito');
+          $uibModalInstance.close('edited');
+        }, function (err) {
+          toastr.error('Ocurrio un error');
+        });
+      }
+
+
+        function canSaveNewProblem() {
+          if(vm.controllerForm.$valid){
+            return true;
+          }
+  		    return false;
+        }
+
+
+
+      	function markAsError() {
+      		var tmpProblem = angular.copy(vm.problem);
+      		tmpProblem.state = PatientProblem.stateChoices.STATE_ERROR;
+          tmpProblem.startDate = moment(tmpProblem.startDate).format('YYYY-MM-DD');
+          if (tmpProblem.closeDate) {
+              tmpProblem.closeDate = moment(tmpProblem.closeDate).format('YYYY-MM-DD');
+
+          }
+
+      		PatientProblem.update(tmpProblem, function (response) {
+          	toastr.success('Problema marcado como error');
+			      $uibModalInstance.close('markedError');
+      		}, function (err) {
+		          toastr.error('Ocurrio un error');
+      		});
+      	}
+
+      function canEdit() {
+        return vm.problem.profesional.id == SessionService.currentUser.id && moment().diff(moment(vm.problem.createdOn), 'hours') <= 8;
+      }
+
+      function canBeClosed() {
+        if(patientProblem.state == 'Closed'){
+          return false;
+        }
+        return true;
+      }
+
+      function canBeMarkedAsError(argument) {
+        return patientProblem.state !== 'Error';
+      }
+
+      function changeStatus() {
+        if(vm.problem.state == 'Active'){
+          vm.problem.endDate = null;
+          vm.startDateCalendarPopup.options.maxDate = new Date();
+        }
+      }
+
+
+    }
+})();
