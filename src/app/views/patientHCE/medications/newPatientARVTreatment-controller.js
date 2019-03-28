63c77034ab5f2728790e1ee5e41b2c1e2a06efd1
Leo <leonardo.manzella@gmail.com>
2019-03-28 11:58:38 -0300

    Fixed Extra decimal values

--- src/app/views/patientHCE/medications/newPatientARVTreatment-controller.js
+++ src/app/views/patientHCE/medications/newPatientARVTreatment-controller.js
@@ -36,6 +36,7 @@
       vm.showInfo = showInfo;
       vm.goToProblems = goToProblems;
       vm.changeStatus = changeStatus;
+      vm.roundNumber = roundNumber;
       vm.patientProblems = [];
       vm.nrtiMedications = [];
       vm.nnrtiMedications = [];
@@ -241,6 +242,11 @@
         }else{
           toastr.error('Ocurrio un error');
         }
+      }
+
+      function roundNumber(number) {
+        console.log(`-- Called round number with ${number} --`);
+        return (Math.round(number*10)/10);
       }
     }
 })();
