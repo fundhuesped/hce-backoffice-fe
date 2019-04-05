78435ca04bf83ed2a82ee0c2a1f446d8e0943ad7
Leo <leonardo.manzella@gmail.com>
2019-04-05 16:57:48 -0300

    Saving HIV details as a JSON field

--- src/app/views/patientHCE/problems/newPatientProblem-controller.js
+++ src/app/views/patientHCE/problems/newPatientProblem-controller.js
@@ -11,7 +11,9 @@
     function newPatientProblemController ($state, HCService, PatientProblem, toastr, moment, Problem, $uibModalInstance) {
 	    var vm = this;
       vm.hceService = HCService;
-      vm.newPatientProblem = {};
+      vm.newPatientProblem = {
+        aditionalData:{}
+      };
       vm.saveNewPatientProblem = saveNewPatientProblem;
       vm.radio = {};
       vm.getProblems = getProblems;
@@ -126,11 +128,11 @@
       }
 
       function hasSelectedTransmission() {
-        return vm.newPatientProblem.vertical || vm.newPatientProblem.mujeres || vm.newPatientProblem.hombres || vm.newPatientProblem.trans || vm.newPatientProblem.inyeccion || vm.newPatientProblem.accidente || vm.newPatientProblem.transfusion
+        return vm.newPatientProblem.aditionalData.vertical || vm.newPatientProblem.aditionalData.mujeres || vm.newPatientProblem.aditionalData.hombres || vm.newPatientProblem.aditionalData.trans || vm.newPatientProblem.aditionalData.inyeccion || vm.newPatientProblem.aditionalData.accidente || vm.newPatientProblem.aditionalData.transfusion
       }
 
       function canSaveNewProblem() {
-        if(vm.controllerForm.$valid && vm.newPatientProblem.clinicalState && hasSelectedTransmission() ){
+        if(vm.controllerForm.$valid && vm.newPatientProblem.aditionalData.clinicalState && hasSelectedTransmission() ){
           return true;
         }
         return false;
@@ -164,6 +166,14 @@
 
         return Problem.getFullActiveList(filters, function(problems){
           vm.problems = problems;
+          if (problems.length <= 0 && !vm.waitingToShowError){
+            toastr.warning('No se han encontrado resultados');
+            vm.waitingToShowError = true;
+            $timeout(
+              function() {
+                vm.waitingToShowError = false;
+              }, 1500);
+          }
         }, displayComunicationError).$promise;
 
       }
